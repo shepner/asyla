a82d79622ddcacb52d7eb0bc187e880fdf5867b9
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-09-01 10:55:00 -0500

    Update update-scripts.sh

--- vmh02/update-scripts.sh
+++ vmh02/update-scripts.sh
@@ -2,8 +2,11 @@
 # https://stackoverflow.com/questions/2466735/how-to-sparsely-checkout-only-one-single-file-from-a-git-repository
 
 git clone --depth 1 --no-checkout --filter=blob:none https://github.com/shepner/asyla.git ~/scripts
-
 cd ~/scripts
 git checkout master -- `hostname -s`
 git checkout master -- vm
+
+find ~/scripts -name "*.sh" -exec chmod 744 {} \;
+
+mv ~/scripts/`hostname -s`/*.sh ~
 
