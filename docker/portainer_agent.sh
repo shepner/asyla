2a5b4d47598168dfc56d19f4a16453886c9d533d
Stephen Hepner <shepner@asyla.org>
2020-09-02 17:46:26 -0500

    automatic update

--- docker/portainer_agent.sh
+++ docker/portainer_agent.sh
@@ -13,32 +13,22 @@
 NAME=portainer_agent
 IMAGE=portainer/agent
 
-# fetch the latest image
-sudo docker pull $IMAGE
-
-# stop the image if it is running
-if [ `sudo docker ps -q -f name=$NAME` ]; then
-  sudo docker stop $NAME
-fi
-
-# remove the stopped image if it exists
-if [ `sudo docker ps -aq --filter "name=$NAME" --filter "status=exited"` ]; then
-  sudo docker rm -v $NAME
-fi
+# Update the image
+dockerContainerUpdate $IMAGE
+# Shut down the old image
+dockerContainerKill $NAME
 
 
-# create the network if needed
+# create the network label
 NETWORK=traefik_net
-if [ ! `sudo docker network ls --quiet --filter "name=$NETWORK"` ]; then
-  sudo docker network create $NETWORK
-fi
+dockerNetworkCreate $NETWORK
 
 # start the image
 sudo docker run --detach --restart=always \
   --name $NAME \
   --cpus=1 \
   --cpu-shares=1024 \
-  --network=traefik_net \
+  --network=$NETWORK \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume /var/lib/docker/volumes:/var/lib/docker/volumes \
   $IMAGE
