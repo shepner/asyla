366573a587c3d9dabf272b8aee9aa383005fd9a8
Stephen Hepner <shepner@asyla.org>
2020-09-03 18:54:51 -0500

    automatic update

--- docker/calibre.sh
+++ docker/calibre.sh
@@ -59,11 +59,11 @@ sudo docker run --detach --restart=always \
   --network=$NETWORK_INTERNET \
   --label traefik.enable=true \
   --label traefik.http.routers.${NAME}_desktop.rule=Host\(\`${NAME}.${MY_DOMAIN}\`\) \
-  --label traefik.http.routers.${NAME}_desktop.entrypoints=web \
+  --label traefik.http.routers.${NAME}_desktop.entrypoints=http \
   --label traefik.http.services.${NAME}_desktop.loadbalancer.server.port=8080 \
+  --label traefik.http.routers.${NAME}_webserver.rule=Host\(\`${NAME}.${MY_DOMAIN}\`\) \
   $IMAGE
 
-#  --label traefik.http.routers.${NAME}_webserver.rule=Host\(\`${NAME}.${MY_DOMAIN}\`\) \
 #  --label traefik.http.routers.${NAME}_webserver.entrypoints=web_secure \
 #  --label traefik.http.services.${NAME}_webserver.loadbalancer.server.port=8081 \
 
