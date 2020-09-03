f1a1a540fa3b12de78fcdccd4118efe13334591a
Stephen Hepner <shepner@asyla.org>
2020-09-03 17:40:47 -0500

    automatic update

--- docker/calibre.sh
+++ docker/calibre.sh
@@ -62,7 +62,14 @@ sudo docker run --detach --restart=always \
   --label traefik.http.routers.${NAME}_desktop.entrypoints=web \
   --label traefik.http.services.${NAME}_desktop.loadbalancer.server.port=8080 \
   --label traefik.http.routers.${NAME}_webserver.rule=Host\(\`${NAME}.${MY_DOMAIN}\`\) \
-  --label traefik.http.routers.${NAME}_webserver.entrypoints.name.address=:8081 \
-  --label traefik.http.services.${NAME}_webserver.loadbalancer.server.port=8081 \
+  --label traefik.http.routers.${NAME}_desktop.entrypoints.${NAME}_desktop.address=":8081" \
+  --label traefik.http.routers.${NAME}_desktop.service=${NAME}_desktop_service \
+  --label traefik.http.services.${NAME}_desktop_service.loadbalancer.server.port=8081 \
   $IMAGE
+
+
+- traefik.http.routers.whoami-https.rule=Host(`whoami.localhost`)
+- traefik.http.routers.whoami-https.entrypoints=web-secure
+- traefik.http.routers.whoami-https.service=whoami-https-service
+- traefik.http.services.whoami-https-service.loadbalancer.server.port=80
 
