2460595e961efda63955271c62bc2964184eddbb
Stephen Hepner <shepner@asyla.org>
2020-09-03 07:48:22 -0500

    automatic update

--- docker/calibre.sh
+++ docker/calibre.sh
@@ -5,29 +5,39 @@
 # 6081(8081) Calibre webserver gui.
 
 # [Customizing calibre](https://manual.calibre-ebook.com/customize.html)
-# Environment variables
-# CALIBRE_OVERRIDE_DATABASE_PATH - allows you to specify the full path to metadata.db. Using this variable you can have metadata.db be in a location other than the library folder. Useful if your library folder is on a networked drive that does not support file locking.
+# Environment variables:
+#   CALIBRE_OVERRIDE_DATABASE_PATH - allows you to specify the full path to metadata.db. Using this variable you can have metadata.db be in a location other than the library folder. Useful if your library folder is on a networked drive that does not support file locking.
+
+
+# Load the functions and environment variables
+. ~/scripts/docker/docker.env
+
 
 NAME=calibre
 IMAGE=linuxserver/calibre
-CONFIGDIR=/docker/$NAME/config
+CONFIGDIR=$DOCKER_DL/$NAME/config
 
-sudo -u docker mkdir -p $CONFIGDIR
 
-sudo docker pull $IMAGE
-sudo docker stop $NAME
-sudo docker rm -v $NAME
+# create the dir if needed
+if [ ! -d $CONFIGDIR ]; then
+  sudo -u \#DOCKER_UID mkdir -p $CONFIGDIR
+fi
 
+
+dockerPull $IMAGE # fetch the latest image
+dockerStopRm $NAME # kill the old one
+
+# load the image
 sudo docker run --detach --restart=always \
   --name $NAME \
-  --cpus=2 \
+  --cpus=1 \
   --cpu-shares=1024 \
-  --env PUID=1003 \
-  --env PGID=1000 \
-  --env TZ="America/Chicago" \
+  --env PUID=$DOCKER_UID \
+  --env PGID=$DOCKER_GID \
+  --env TZ=$LOCAL_TZ \
   --env CALIBRE_OVERRIDE_DATABASE_PATH="/config/metadata.db" \
   --mount type=bind,src=$CONFIGDIR,dst=/config \
-  --mount type=bind,src=/mnt/nas/data1/media,dst=/media \
+  --mount type=bind,src=$DATA1/media,dst=/media \
   --publish published=6080,target=8080,protocol=tcp,mode=ingress \
   --publish published=6081,target=8081,protocol=tcp,mode=ingress \
   $IMAGE
--- docker/portainer.sh
+++ docker/portainer.sh
@@ -7,24 +7,31 @@
 
 
 # Load functions and environment variables
-. ~/scripts/docker/docker.env
+#. ~/scripts/docker/docker.env
 
 
-NAME=portainer
-IMAGE=portainer/portainer
+cd /tmp
+curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+sudo docker stack deploy --compose-file=portainer-agent-stack.yml portainer
+sudo rm portainer-agent-stack.yml
+
+
+
+#NAME=portainer
+#IMAGE=portainer/portainer
 
 
 # Update the image
-dockerServiceUpdate $NAME
+#dockerServiceUpdate $NAME
 
 
 # create the network label
-NETWORK=portainer_agent_network
-dockerServiceNetworkCreate $NETWORK
+#NETWORK=portainer_agent_network
+#dockerServiceNetworkCreate $NETWORK
 
 # create the data volume
-VOLUME=portainer_data
-dockerVolumeCreate portainer_data
+#VOLUME=portainer_data
+#dockerVolumeCreate portainer_data
 
 
 # create the network label
@@ -32,14 +39,14 @@ dockerVolumeCreate portainer_data
 #dockerNetworkCreate $NETWORK
 
 # start the image
-sudo docker service create \
-  --name $NAME \
-  --replicas=1 \
-  --network=$NETWORK \
-  --publish 9000:9000 \
-  --mount type=bind,src=$VOLUME,dst=/data \
-  --label traefik.enable=true \
-  --label traefik.http.routers.portainer.entrypoints=web \
-  --label traefik.http.routers.portainer.rule=Host\(\`$NAME.$MY_DOMAIN\`\) \
-  $IMAGE -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+#sudo docker service create \
+#  --name $NAME \
+#  --replicas=1 \
+#  --network=$NETWORK \
+#  --publish 9000:9000 \
+#  --mount type=bind,src=$VOLUME,dst=/data \
+#  --label traefik.enable=true \
+#  --label traefik.http.routers.portainer.entrypoints=web \
+#  --label traefik.http.routers.portainer.rule=Host\(\`$NAME.$MY_DOMAIN\`\) \
+#  $IMAGE -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
 
