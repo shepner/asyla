4adddcf2e63139dab54e3926ee3d2ffa3334d499
Stephen Hepner <shepner@asyla.org>
2020-09-03 21:41:37 -0500

    automatic update

--- docker/refresh_all.sh
+++ docker/refresh_all.sh
@@ -3,11 +3,19 @@
 # update all of the configs
 ~/update_scripts.sh
 
+
 # reload all the containers
+HOSTNAME=`hostname -s`
+
+if [ ${HOSTNAME} -eq "d01" ]; then
 ~/scripts/docker/traefik/traefik.sh
 
 ~/scripts/docker/calibre.sh
 ~/scripts/docker/booksonic.sh
+elif [ ${HOSTNAME} -eq "d02" ]; then
 
+elif [ ${HOSTNAME} -eq "d03" ]; then
+
+fi
 
 
