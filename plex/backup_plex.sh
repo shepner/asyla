cd3a6d299b1f6a4591c7bde205ce1516a7e4286e
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-09-11 15:51:16 -0500

    Update backup_plex.sh

--- plex/backup_plex.sh
+++ plex/backup_plex.sh
@@ -2,12 +2,9 @@
 
 sudo service plexmediaserver stop
 
-# need to use `tar` because rsync throws errors
-#sudo sudo tar -cvf /mnt/nas/docker1/plex.backup/plexmediaserver.tar -C /var/lib/ plexmediaserver
-#sudo sudo tar -cvf /mnt/nas/docker1/plex.backup/plexmediaserver.tar -C /var/lib/ plexmediaserver
-sudo sudo tar -cvf /mnt/nas/data1/docker/plex.backup/plexmediaserver.tar -C /var/lib/ plexmediaserver
+sudo sudo tar -v -czf /mnt/nas/data1/docker/plexmediaserver.tgz -C /var/lib/ plexmediaserver
 
 # To restore:
-#sudo tar -xvf /mnt/nas/data1/docker/plex.backup/plexmediaserver.tar -C /var/lib/
+#sudo tar -v -xzf /mnt/nas/data1/docker/plexmediaserver.tgz -C /var/lib/
 
 sudo service plexmediaserver start
