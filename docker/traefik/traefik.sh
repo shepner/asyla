a62f6d37d314df2782a7b775d3147c33d3a7ab7b
Stephen Hepner <shepner@asyla.org>
2020-09-14 07:43:28 -0500

    automatic update

--- docker/traefik/traefik.sh
+++ docker/traefik/traefik.sh
@@ -17,8 +17,8 @@ fi
 
 # Copy the Traefik support files
 sudo -u \#${DOCKER_UID} cp ~/scripts/docker/${NAME}/${NAME}.yaml $DOCKER_D2/$NAME
-sudo -u \#${DOCKER_UID} cp ~/scripts/docker/${NAME}/users_credentials $DOCKER_D2
-sudo -u \#${DOCKER_UID} chmod 500 $DOCKER_D2/users_credentials
+sudo -u \#${DOCKER_UID} cp ~/scripts/docker/${NAME}/users_credentials $DOCKER_D2/${NAME}
+sudo -u \#${DOCKER_UID} chmod 400 $DOCKER_D2/${NAME}/users_credentials
 
 dockerNetworkCreate_general
 
