2286f7321adb3404f7d470a4c02693f8b67f0d02
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-10-15 15:51:18 -0500

    Create jupyter-custom.sh

---
+++ docker/jupyter/custom/jupyter-custom.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# https://jupyter-docker-stacks.readthedocs.io/en/latest/
+
+# Load the functions and environment variables
+. ~/scripts/docker/common.sh
+
+
+NAME=jupyter-custom
+IMAGE=${NAME}
+SOURCE=~/scripts/docker/jupyter/custom
+DOCKERDIR=${DOCKER_D1}
+DOCKERAPPDIR=${DOCKERDIR}/${NAME}
+CONFIGDIR=${DOCKERAPPDIR}/config
+
+
+# build the container
+cd ${SOURCE}
+build --tag $IMAGE .
+
+#
+dockerPull ${IMAGE} # fetch the latest image
+dockerStopRm ${NAME} # kill the old one
+#
+# create the dir if needed
+if [ ! -d ${CONFIGDIR} ]; then
+  sudo -u \#${DOCKER_UID} mkdir -p ${CONFIGDIR}
+fi
+#
+echo "Making a backup"
+sudo -u \#${DOCKER_UID} tar -czf ${DOCKER_D1}/${NAME}.tgz -C ${DOCKERDIR} ${NAME}
+echo "Backup complete"
+
+sudo docker run --detach --restart=unless-stopped \
+  --name ${NAME} \
+  --cpu-shares=1024 \
+  --env PUID=${DOCKER_UID} \
+  --env PGID=${DOCKER_GID} \
+  --env TZ=${LOCAL_TZ} \
+  --mount type=bind,src=${CONFIGDIR},dst=/config \
+  --publish published=8888,target=8888,protocol=tcp,mode=ingress \
+  ${IMAGE}
+
