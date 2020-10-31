88e10aeb65af9c00d2213cd338b397ab9b157133
Stephen Hepner <shepner@asyla.org>
2020-10-31 10:01:31 -0500

    automatic update

--- docker/unifi-controller.sh
+++ docker/unifi-controller.sh
@@ -2,15 +2,15 @@
 # https://docs.linuxserver.io/images/docker-unifi-controller
 
 # [UniFi - Ports Used](https://help.ui.com/hc/en-us/articles/218506997)
+# 1900/udp Required for Make controller discoverable on L2 network option
 # 3478/udp Unifi STUN port
-# 10001/udp Required for AP discovery
+# 5514 Remote syslog port
+# 6789 For mobile throughput test
 # 8080 Required for device communication
 # 8443 Unifi web admin port
-# 1900/udp Required for Make controller discoverable on L2 network option
 # 8843 Unifi guest portal HTTPS redirect port
 # 8880 Unifi guest portal HTTP redirect port
-# 6789 For mobile throughput test
-# 5514 Remote syslog port
+# 10001/udp Required for AP discovery
 
 
 # Load the functions and environment variables
@@ -42,13 +42,14 @@ sudo docker run --detach --restart=unless-stopped \
   --env PGID=${DOCKER_GID} \
   --env TZ=${LOCAL_TZ} \
   --mount type=bind,src=${CONFIGDIR},dst=/config \
+  --publish published=1900,target=1900,protocol=udp,mode=ingress \
   --publish published=3478,target=3478,protocol=udp,mode=ingress \
   --publish published=5514,target=5514,protocol=udp,mode=ingress \
+  --publish published=6789,target=6789,protocol=tcp,mode=ingress \
   --publish published=8080,target=8080,protocol=tcp,mode=ingress \
   --publish published=8443,target=8443,protocol=tcp,mode=ingress \
-  --publish published=8880,target=8880,protocol=tcp,mode=ingress \
   --publish published=8843,target=8843,protocol=tcp,mode=ingress \
-  --publish published=6789,target=6789,protocol=tcp,mode=ingress \
+  --publish published=8880,target=8880,protocol=tcp,mode=ingress \
   --publish published=10001,target=10001,protocol=udp,mode=ingress \
   ${IMAGE}
 
