0a518bfbfcbe9c66c95b3763dba48c6a7913c329
Stephen Hepner <shepner@asyla.org>
2020-11-23 14:38:04 -0600

    automatic update

---
+++ docker/swag.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-swag
+
+# https://blog.linuxserver.io/2020/08/21/introducing-swag/
+# https://docs.linuxserver.io/general/swag
+
+
+# Load the functions and environment variables
+. ~/scripts/docker/common.sh
+
+
+NAME=`basename "${0}" ".sh"`
+IMAGE=ghcr.io/linuxserver/${NAME}
+DOCKERDIR=${DOCKER_DL} # Run this locally
+DOCKERAPPDIR=${DOCKERDIR}/${NAME}
+CONFIGDIR=${DOCKERAPPDIR}/config
+#
+dockerPull ${IMAGE} # fetch the latest image
+dockerStopRm ${NAME} # kill the old one
+#
+# create the dir if needed
+if [ ! -d ${CONFIGDIR} ]; then
+  sudo -u \#${DOCKER_UID} mkdir -p ${CONFIGDIR}
+fi
+#
+echo "Making a backup"
+sudo -u \#${DOCKER_UID} tar -czf ${DOCKER_D1}/${NAME}.tgz -C ${DOCKERDIR} ${NAME}
+echo "Backup complete"
+
+sudo docker run --detach --restart=unless-stopped \
+  --name ${NAME} \
+  --cpu-shares=1024 \
+  --env PUID=${DOCKER_UID} \
+  --env PGID=${DOCKER_GID} \
+  --env TZ=${LOCAL_TZ} \
+  --cap-add=NET_ADMIN \
+  --env URL=asyla.org \
+  --env SUBDOMAINS=www, \
+  --env VALIDATION=dns \
+  --env DNSPLUGIN=cloudflare \
+  --env EMAIL= `shepner@asyla.org` \
+  --env ONLY_SUBDOMAINS=false \
+  --env STAGING=false \
+  --mount type=bind,src=${CONFIGDIR},dst=/config \
+  --publish published=10080,target=8080,protocol=tcp,mode=ingress \
+  ${IMAGE}
+
