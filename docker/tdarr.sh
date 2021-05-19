d6b16dec1370a8243c9b081d4219c8e2c072bca7
Stephen Hepner <shepner@asyla.org>
2021-05-19 07:36:27 -0500

    automatic update

--- docker/tdarr.sh
+++ docker/tdarr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # http://tdarr.io/docs/installation/docker
 # http://tdarr.io/docs/installation/getting-started
-# This is for the tdar node
+# This is for the tdar server
 
 
 # Load the global functions and default environment variables
@@ -13,8 +13,7 @@ IMAGE=haveagitgat/${NAME}
 #DOCKERDIR=${DOCKER_DL} # local disk
 DOCKERDIR=${DOCKER_D1} # NFS attached HDD
 #DOCKERDIR=${DOCKER_D2} # NFS attached SSD
-#DOCKERAPPDIR=${DOCKERDIR}/${NAME}
-DOCKERAPPDIR=${DOCKERDIR}/tdarr
+DOCKERAPPDIR=${DOCKERDIR}/${NAME}
 CONFIGDIR=${DOCKERAPPDIR}/configs
 
 
@@ -23,6 +22,7 @@ dockerPull ${IMAGE} # fetch the latest image
 dockerStopRm ${NAME} # kill the old one
 dockerNetworkCreate ${NETWORK} # create the network if needed
 appCreateDir ${CONFIGDIR} # create the config folder if needed
+appCreateDir ${DOCKERAPPDIR}/server
 appCreateDir ${DOCKERAPPDIR}/logs
 appCreateDir ${DOCKERAPPDIR}/transcode_cache
 #appBackup ${DOCKERDIR} ${NAME} # backup the app
@@ -30,22 +30,22 @@ appCreateDir ${DOCKERAPPDIR}/transcode_cache
 
 sudo docker run --detach --restart=unless-stopped \
   --name ${NAME} \
-  --env nodeID=MyFirstTdarrNode \
 `:  --cpus=2` \
 `:  --cpu-shares=768` \
   --env TZ=${LOCAL_TZ} \
   --env PUID=${DOCKER_UID} \
   --env PGID=${DOCKER_GID} \
+  --mount type=bind,src=${DOCKERAPPDIR}/server,dst=/app/server \
   --mount type=bind,src=${CONFIGDIR},dst=/configs \
   --mount type=bind,src=${DOCKERAPPDIR}/logs,dst=/app/logs \
   --mount type=bind,src=${DOCKERAPPDIR}/transcode_cache,dst=/tmp \
   --mount type=bind,src=/mnt/nas/data1/media/Videos/00-Handbrake,dst=/media \
-  --env nodeIP=0.0.0.0 \
-  --env nodePort=8267 \
   --env serverIP=0.0.0.0 \
   --env serverPort=8266 \
+  --env webUIPort=8265 \
   --network=${NETWORK} \
-  --publish published=8267,target=8267,protocol=tcp,mode=ingress \
+  --publish published=8265,target=8265,protocol=tcp,mode=ingress \
+  --publish published=8266,target=8266,protocol=tcp,mode=ingress \
 `:  --env NVIDIA_DRIVER_CAPABILITIES=all` \
 `:  --env NVIDIA_VISIBLE_DEVICES=all` \
 `:  --gpus=all` \
