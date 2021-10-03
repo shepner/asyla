2c784c38a783be7d53fb6b5e99ee827b58be5e15
Stephen Hepner <shepner@asyla.org>
2021-10-02 20:08:27 -0500

    automatic update

--- docker/pihole.sh
+++ docker/pihole.sh
@@ -47,19 +47,19 @@ sudo docker run --detach --restart=unless-stopped \
 dockerRestartProxy
 
 
-printf 'Starting up pihole container '
-for i in $(seq 1 20); do
-    if [ "$(docker inspect -f "{{.State.Health.Status}}" ${NAME})" == "healthy" ] ; then
-        printf ' OK'
-        echo -e "\n$(docker logs ${NAME} 2> /dev/null | grep 'password:') for your pi-hole: https://${IP}/admin/"
-        exit 0
-    else
-        sleep 3
-        printf '.'
-    fi
+#printf 'Starting up pihole container '
+#for i in $(seq 1 20); do
+#    if [ "$(docker inspect -f "{{.State.Health.Status}}" ${NAME})" == "healthy" ] ; then
+#        printf ' OK'
+#        echo -e "\n$(docker logs ${NAME} 2> /dev/null | grep 'password:') for your pi-hole: https://${IP}/admin/"
+#        exit 0
+#    else
+#        sleep 3
+#        printf '.'
+#    fi
 
-    if [ $i -eq 20 ] ; then
-        echo -e "\nTimed out waiting for Pi-hole start, consult your container logs for more info (\`docker logs pihole\`)"
-        exit 1
-    fi
-done;
+#    if [ $i -eq 20 ] ; then
+#        echo -e "\nTimed out waiting for Pi-hole start, consult your container logs for more info (\`docker logs pihole\`)"
+#        exit 1
+#    fi
+#done;
