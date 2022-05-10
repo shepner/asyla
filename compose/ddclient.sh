ff79c5836c21c4e3bfc4ed5d8d0e400dcf8fcf80
Stephen Hepner <shepner@asyla.org>
2022-05-10 18:10:41 -0500

    automatic update

--- compose/ddclient.sh
+++ compose/ddclient.sh
@@ -8,34 +8,26 @@ NAME=`basename "${0}" ".sh"`
 BASEDIR=$(dirname "$0")
 
 
+# docker
+#
+doas docker compose -f ${BASEDIR}/${NAME}.yml up --detach
+#
+#doas docker compose -f ./scripts/compose/ddclient.yml ps
+#doas docker compose -f ./scripts/compose/ddclient.yml down --volumes
+#doas docker compose -f ./scripts/compose/ddclient.yml push
 
+
+
+# swarm
 # https://docs.docker.com/engine/swarm/stack-deploy/
-
-
+#
 # run the `docker service` and `docker stack` commands on the swarm master node
 #doas docker node ls
-
-
-# Start the registry as a service on your swarm
-#doas docker service create --name registry --publish published=5000,target=5000 registry:2
-#doas docker service ls
-
-
-# run in docker
-doas docker compose -f ${BASEDIR}/${NAME}.yml up --detach
-#doas docker compose -f ./scripts/swarm/ddclient.yml ps
-#doas docker compose -f ./scripts/swarm/ddclient.yml down --volumes
-#doas docker compose -f ./scripts/swarm/ddclient.yml push
-
-
-
-# run on swarm 
-#docker stack deploy --compose-file name1.yaml --compose-file name2.yaml ${NAME}
+#
+#doas docker stack deploy --compose-file name1.yaml --compose-file name2.yaml ${NAME}
 #doas docker stack deploy --compose-file ${BASEDIR}/${NAME}.yml ${NAME}
+#
 #doas docker service ls
 #doas docker service scale ddclient_ddclient=0
 #doas docker service rm ${NAME}
-
-
-
 
--- compose/torrent.sh
+++ compose/torrent.sh
@@ -8,40 +8,39 @@ NAME=`basename "${0}" ".sh"`
 BASEDIR=$(dirname "$0")
 
 
+# docker
+#
+doas docker compose -f ${BASEDIR}/${NAME}.yml up --detach
+#
+#doas docker compose -f ./scripts/compose/torrent.yml ps
+#doas docker compose -f ./scripts/compose/torrent.yml down --volumes
+#doas docker compose -f ./scripts/compose/torrent.yml push
 
+
+# swarm
 # https://docs.docker.com/engine/swarm/stack-deploy/
-
-
+#
 # run the `docker service` and `docker stack` commands on the swarm master node
 #doas docker node ls
-
-
-# Start the registry as a service on your swarm
-#doas docker service create --name registry --publish published=5000,target=5000 registry:2
-#doas docker service ls
-
-
-# start the app
-#doas docker compose -f ${BASEDIR}/${NAME}.yml up --detach
-#doas docker compose -f ./scripts/swarm/ddclient.yml ps
-#doas docker compose -f ./scripts/swarm/ddclient.yml down --volumes
-#doas docker compose -f ./scripts/swarm/ddclient.yml push
-
-
-
+#
 #doas docker stack deploy --compose-file name1.yaml --compose-file name2.yaml ${NAME}
-doas docker stack deploy --compose-file ${BASEDIR}/${NAME}.yml ${NAME}
+#doas docker stack deploy --compose-file ${BASEDIR}/${NAME}.yml ${NAME}
+#
 #doas docker service ls
-
-# to access the shell, find the node and run:
-#doas docker exec -it ${ID} bash
-
+#doas docker service scale ddclient_ddclient=0
+#doas docker service rm ${NAME}
+#
+#
 #doas docker service scale torrent_sonarr=0
 #doas docker service rm torrent_sonarr
-
+#
 #doas docker service scale torrent_jackett=0
 #doas docker service rm torrent_jackett
-
+#
 #doas docker service scale torrent_transmission=0
 #doas docker service rm torrent_transmission
+#
+#
+# to access the shell, find the node and run:
+#doas docker exec -it ${ID} bash
 
