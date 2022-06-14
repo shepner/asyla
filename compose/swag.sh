5f4a0fe6693536b6dd749eb21be4562591116ceb
Stephen Hepner <shepner@asyla.org>
2022-06-14 10:25:48 -0500

    automatic update

--- compose/swag.sh
+++ compose/swag.sh
@@ -54,10 +54,10 @@ sudo docker network connect calibre_net ${NAME}
 #sudo docker network connect codimd_net ${NAME}
 #sudo docker network connect dillinger_net ${NAME}
 sudo docker network connect heimdall_net ${NAME}
-#sudo docker network connect jackett_net ${NAME}
+sudo docker network connect jackett_net ${NAME}
 sudo docker network connect jupyter_net ${NAME}
-#sudo docker network connect sonarr_net ${NAME}
-#sudo docker network connect transmission_net ${NAME}
+sudo docker network connect sonarr_net ${NAME}
+sudo docker network connect transmission_net ${NAME}
 sudo docker network connect unifi-controller_net ${NAME}
 
 # Dont forget to also setup a config file per app:
