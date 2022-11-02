d6880fdcaf5e04452699154170cdf9030aaae946
Stephen Hepner <stephen.hepner@kohls.com>
2022-11-01 20:48:30 -0500

    automatic update

---
+++ docker/homebridge.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# https://github.com/homebridge/homebridge/wiki/Install-Homebridge-on-Docker
+# https://github.com/oznu/docker-homebridge
+# https://homebridge.io/
+
+
+# Load the global functions and environment variables
+. ~/scripts/docker/common.sh
+
+
+# Setup the app specific environment vars
+IMAGE=oznu/${NAME}:latest
+DOCKERDIR=${DOCKER_DL} # local disk
+#DOCKERDIR=${DOCKER_D1} # NFS attached HDD
+#DOCKERDIR=${DOCKER_D2} # NFS attached SSD
+DOCKERAPPDIR=${DOCKERDIR}/${NAME}
+CONFIGDIR=${DOCKERAPPDIR}/config
+
+
+# Perform setups/updates as needed
+dockerPull ${IMAGE} # fetch the latest image
+dockerStopRm ${NAME} # kill the old one
+dockerNetworkCreate ${NETWORK} # create the network if needed
+appCreateDir ${DOCKERAPPDIR} # create the folder if needed
+#appCreateDir ${CONFIGDIR} # create the folder if needed
+appBackup ${DOCKERDIR} ${NAME} # backup the app
+
+
+sudo docker run --detach --restart=unless-stopped \
+  --name ${NAME} \
+  --cpus=2 \
+  --env TZ=${LOCAL_TZ} \
+  --env ENABLE_AVAHI=1 \
+  --network=${NETWORK} \
+`:  --network=host` \
+  --mount type=bind,src=${DOCKERAPPDIR},dst=/homebridge \
+`:  --publish published=8581,target=8581,protocol=tcp,mode=ingress` \
+  ${IMAGE}
+
+dockerRestartProxy
+
