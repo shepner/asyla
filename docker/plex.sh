e47fe91eb85604c86da9c6abbfbabae6d8bfe927
Stephen Hepner <stephen.hepner@kohls.com>
2022-11-24 12:00:37 -0600

    automatic update

---
+++ docker/plex.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-plex
+
+
+# Load the global functions and environment variables
+. ~/scripts/docker/common.sh
+
+
+# Setup the app specific environment vars
+IMAGE=ghcr.io/linuxserver/${NAME}
+DOCKERDIR=${DOCKER_DL} # local disk
+#DOCKERDIR=${DOCKER_D1} # NFS attached HDD
+#DOCKERDIR=${DOCKER_D2} # NFS attached SSD
+DOCKERAPPDIR=${DOCKERDIR}/${NAME}
+CONFIGDIR=${DOCKERAPPDIR}/config
+
+
+# Perform setups/updates as needed
+dockerPull ${IMAGE} # fetch the latest image
+dockerStopRm ${NAME} # kill the old one
+#dockerNetworkCreate ${NETWORK} # create the network if needed
+appCreateDir ${DOCKERAPPDIR} # create the folder if needed
+#appCreateDir ${CONFIGDIR} # create the folder if needed
+appBackup ${DOCKERDIR} ${NAME} # backup the app
+
+
+doas docker run --detach --restart=unless-stopped \
+  --name ${NAME} \
+`:  --cpus=2` \
+`:  --cpu-shares=1024` `# default job priority` \
+  --env PUID=${DOCKER_UID} \
+  --env PGID=${DOCKER_GID} \
+  --env TZ=${LOCAL_TZ} \
+  --env VERSION=latest \
+  --env PLEX_CLAIM= `#optional` \
+  --env DOCKER_MODS=linuxserver/mods:plex-absolute-hama \
+`:  --network=${NETWORK}` \
+  --network=host \
+  --env CMD_DOMAIN=${NAME}.${MY_DOMAIN} \
+  --env CMD_PROTOCOL_USESSL=true \
+  --mount type=bind,src=${DOCKERAPPDIR},dst=/config \
+  --mount type=bind,src=/mnt/nas/data1/media,dst=/mnt/nas/data1/media \
+`:  --publish published=32400,target=32400,protocol=tcp,mode=ingress` \
+  ${IMAGE}:latest
+
+dockerRestartProxy
+
--- docker/refresh_all.sh
+++ docker/refresh_all.sh
@@ -20,14 +20,15 @@ if [ ${HOSTNAME} = "d01" ]; then
   ~/scripts/docker/calibre.sh
   ~/scripts/docker/booksonic.sh
   
-  #~/scripts/docker/transmission.sh
-  #~/scripts/docker/jackett.sh
-  #~/scripts/docker/sonarr.sh
+  ~/scripts/docker/transmission.sh
+  ~/scripts/docker/jackett.sh
+  ~/scripts/docker/sonarr.sh
 
   ~/scripts/docker/swag.sh  # run this last
 elif [ ${HOSTNAME} = "d02" ]; then
   echo ${HOSTNAME}
   # ~/scripts/docker/foldingathome.sh
+  ~/scripts/docker/plex.sh
 
 elif [ ${HOSTNAME} = "d03" ]; then
   echo ${HOSTNAME}
