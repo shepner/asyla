64e38e0848f80b3708122255616357cdaee69e5d
Stephen Hepner <stephen.hepner@kohls.com>
2022-11-29 08:48:45 -0600

    automatic update

--- docker/refresh_all.sh
+++ docker/refresh_all.sh
@@ -13,7 +13,9 @@ if [ ${HOSTNAME} = "d01" ]; then
 
   #~/scripts/docker/ddclient.sh
   ~/scripts/docker/heimdall.sh
+
   ~/scripts/docker/unifi-controller.sh
+  ~/scripts/docker/homebridge.sh
   
   ~/scripts/docker/jupyter/jupyter.sh
 
